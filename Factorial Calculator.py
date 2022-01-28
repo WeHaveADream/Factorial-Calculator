05d0aa48f7546ffa0d0b51b815569124b4730602
WeHaveADream <71896051+WeHaveADream@users.noreply.github.com>
2022-01-28 14:10:04 -0300

    Add files via upload

---
+++ Factorial Calculator.py
@@ -0,0 +1,10 @@
+#/Factorial Calculator
+#/Calculadora De Fatoriais
+
+numero = int(input('digite um numero: '))
+fatorial=numero
+contador = 1
+while (numero-contador)>1:
+    fatorial=fatorial*(numero-contador)
+    contador+=1
+print('{0}! = {1}'.format(numero,fatorial))
